18de28d58eddc2a7f6f719bf159e7457e6250a77
Timothy Sabat <timothy.sabat@gmail.com>
2012-08-24 14:58:18 -0700

    the whole thing

---
+++ setup.sh
@@ -0,0 +1,37 @@
+virtualenv .
+source bin/activate
+
+pip install -e git+git@github.com:Doula/Doula.git@ra#egg=doula
+pip install -e git+git@github.com:Doula/Bambino.git@master#egg=bambino
+
+mkdir /opt/webapp
+sudo mkdir -p /etc/supervisor/conf.d
+
+sudo chown $USER:admin /opt/webapp
+sudo chown $USER:admin /etc/supervisor/conf.d
+
+pushd .
+
+cd /opt/webapp
+
+for x in anonweb createweb anweb userweb svysvc acsvc admintools
+do
+    echo "cloning $x"
+    git clone http://code.corp.surveymonkey.com/doula/$x.git /opt/webapp/$x
+    pushd .
+        cd $x
+        folder=${PWD##*/}
+        git submodule init
+        git submodule update
+        pushd .
+            branch=$(git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3)
+            cd etc
+            git checkout $branch
+            git pull origin $branch
+            ln -s $(pwd)/supervisor.conf /etc/supervisor/conf.d/$folder.conf
+        popd
+    popd
+done
+
+popd
+osascript start_sessions.scpt $(pwd)
